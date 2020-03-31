beb6be112518fa71cb66f8543ae63ef30b10e66e
zhupengwei <zhupengwei@aliyun.com>
2020-03-31 17:44:38 +0800

    初始化项目

---
+++ src/pers/zpw/demo.java
@@ -0,0 +1,13 @@
+package pers.zpw;
+
+/**
+ *
+ * @author ZhuPengWei
+ * @email pengwei@mgtv.com
+ * @date 2020-03-31 17:43
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
